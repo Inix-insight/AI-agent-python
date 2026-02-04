b3ec3173861d57b222c4e911ccd9e0c531cf643d
Inix-insight <jasonlouhe@hotmail.com>
2026-02-04 15:32:43 +0800

    2-4: updated get_file... functions and added write_file

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,13 +2,20 @@ from os import path
 import os
 
 def get_files_info(working_directory, directory = "."):
-    working_absolute_path = path.abspath(working_directory)
-    full_path = path.normpath(path.join(working_absolute_path, directory))
-    if path.commonpath([full_path, working_absolute_path]) != working_absolute_path:
-        return f"Error: Cannot list \"{directory}\" as it is outside the permitted working directory\n"
-    if not path.isdir(full_path):
-        return f"Error: \"{directory}\" is not a directory\n"
-    format = ""
-    for item in os.listdir(full_path):
-        format += f"  - {item}: file_size={path.getsize(path.join(full_path, item))} bytes, is_dir={path.isdir((path.join(full_path, item)))}\n"
-    return format
+    try:
+        working_absolute_path = path.abspath(working_directory)
+        full_path = path.normpath(path.join(working_absolute_path, directory))
+        
+        if path.commonpath([full_path, working_absolute_path]) != working_absolute_path:
+            return f"Error: Cannot list \"{directory}\" as it is outside the permitted working directory\n"
+        
+        if not path.isdir(full_path):
+            return f"Error: \"{directory}\" is not a directory\n"
+        
+        format = ""
+        for item in os.listdir(full_path):
+            format += f"  - {item}: file_size={path.getsize(path.join(full_path, item))} bytes, is_dir={path.isdir((path.join(full_path, item)))}\n"
+        return format
+    
+    except Exception as e:
+        return f"Error: {str(e)}"
---
+++ functions/write_file.py
@@ -0,0 +1,21 @@
+from os import path
+import os
+
+def write_file(working_directory, file_path, content):
+    try:
+        working_absolute_path = path.abspath(working_directory)
+        full_path = path.normpath(path.join(working_absolute_path, file_path))
+        
+        if path.commonpath([full_path, working_absolute_path]) != working_absolute_path:
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory\n'
+        
+        if path.isdir(full_path):
+            return f'Error: Cannot write to "{file_path}" as it is a directory'
+        
+        os.makedirs(path.dirname(full_path), exist_ok = True)
+        file = open(full_path, "w")
+        file.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    
+    except Exception as e:
+        return f"Error: {str(e)}"
